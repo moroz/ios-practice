345808689c9b57657329876a99403a2a21d38426
Karol Moroz <k.j.moroz@gmail.com>
2022-05-20 10:18:59 +0200

    Add more notes

--- HelloWorld.playground/Contents.swift
+++ HelloWorld.playground/Contents.swift
@@ -1,8 +1,33 @@
 import UIKit
 
+// var defines a mutable variable
+// String type is called String
 var greeting = "Hello, playground"
 
+// let defines an immutable variable
+// Defaults to Int type (i32?)
 let x = 10
 let y = 20
 
 x + y
+
+let double = 21.37
+
+let uppercaseGreeting = greeting.uppercased()
+
+let reversed = String("deified".reversed())
+
+func isPalindrome(_ str: String) -> Bool {
+    let reversed = String(str.reversed())
+    return reversed == str
+}
+
+isPalindrome("deified")
+
+isPalindrome("abacad")
+
+// convert any (?) value to String
+String(21.37)
+
+// string interpolation
+let msg = "How much is a bottle of cheap wine? \(double) PLN"
