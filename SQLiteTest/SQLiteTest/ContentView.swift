6babe8a4ef1ee42f41cc4a981b0d0ffb04b5848b
Karol Moroz <karol@moroz.dev>
2022-06-01 22:56:47 +0200

    Add SQLiteTest project

---
+++ SQLiteTest/SQLiteTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SQLiteTest
+//
+//  Created by Karol Moroz on 01/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SQLiteTest/SQLiteTest/DataStore.swift
@@ -0,0 +1,9 @@
+//
+//  DataStore.swift
+//  SQLiteTest
+//
+//  Created by Karol Moroz on 01/06/2022.
+//
+
+import Foundation
+import SQLite3
